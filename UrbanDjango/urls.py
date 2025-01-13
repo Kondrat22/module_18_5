8d1c24b43d9c68d4084d39a3b491b4e9bf7e03b5
Vladimir <kondrat_va@bk.ru>
2025-01-13 22:24:10 +0300

    add class shablon

--- UrbanDjango/urls.py
+++ UrbanDjango/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from task2.views import index
+from task2.views import index, index2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('func', index), path('class', index2.as_view())
 ]
--- task2/views.py
+++ task2/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
 def index(request):
     return render(request, 'task2/index.html')
+
+class index2(TemplateView):
+    template_name = 'task2/index2.html'
+
